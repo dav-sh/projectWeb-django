a4aa0fb824b360d5707585d548dc31782bfd0901
dav-sh <dav.llwyr@gmail.com>
2023-10-24 22:08:16 -0600

    I can filter by category, but I need to be careful with the variable's name

--- Blog/urls.py
+++ Blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Blog, name='blog'),
-
+    path('category/<int:category_id>/',views.category, name='category')
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 # Create your views here.
 def Blog(request):
-    blogs = Post.objects.all()
-    return render(request, 'blog/blog.html', {'blogs':blogs})
+    posts = Post.objects.all()
+    categories = Category.objects.all()
+    print(posts)
+    return render(request, 'blog/blog.html', {'posts':posts,'categories':categories})
+
+def category(request, category_id:int):
+    categoria = Category.objects.get(id=category_id)
+
+    posts = Post.objects.filter(category = categoria)
+    print(posts)
+    return render(request, 'blog/category.html', {'posts':posts,'categories':category})
