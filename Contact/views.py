b05f8ba45b98f01118929e543d6d2c476517663c
dav-sh <dav.llwyr@gmail.com>
2023-10-28 14:58:57 -0600

    I added the code to send the email, but I need to configure the user and pw

--- Contact/views.py
+++ Contact/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Contact
 from .forms import ContactForm
-
+from django.core.mail import EmailMessage
 
 # Create your views here.
 def contact(request):
@@ -14,6 +14,12 @@ def contact(request):
             email = request.POST.get('your_email')
             message = request.POST.get('your_message')
             # If I reach this point I gonna redirect the user to the same page but adding the 'value' just to know if it's valid
-            return redirect('/contact/?value')
+            mail = EmailMessage('msg from django: '+name,message + ' email: '+email,"",'lvl1noob1@gmail.com',reply_to=[email])
+            try:
+                mail.send()
+                return redirect('/contact/?value')
+            except:
+                return redirect('/contact/?invalid')
+            
 
     return render(request, 'contact/contact.html',{'my_form':contc_form})
--- projectweb/settings.py
+++ projectweb/settings.py
@@ -128,3 +128,11 @@ MEDIA_ROOT =  BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+#EMAIL CONFIGURATION
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER="lvl1noob1@gmail.com"
+EMAIL_HOST_PASSWORD='pw'
