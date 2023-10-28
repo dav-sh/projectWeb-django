a12632db39bd2748c87c696e0b43a36875ead097
dav-sh <dav.llwyr@gmail.com>
2023-10-28 14:29:33 -0600

    I already caught the data from the POST

--- Contact/views.py
+++ Contact/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Contact
 from .forms import ContactForm
+
 
 # Create your views here.
 def contact(request):
     contc_form = ContactForm()
     # ctc = Contact.objects.all()
+    if request.method == "POST":
+        contc_form = ContactForm(data=request.POST)
+        if contc_form.is_valid():
+            name = request.POST.get('your_name')
+            email = request.POST.get('your_email')
+            message = request.POST.get('your_message')
+            # If I reach this point I gonna redirect the user to the same page but adding the 'value' just to know if it's valid
+            return redirect('/contact/?value')
+
     return render(request, 'contact/contact.html',{'my_form':contc_form})
