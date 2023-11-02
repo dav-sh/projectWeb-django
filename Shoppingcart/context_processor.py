095ca48fbef56e3e1cbd7c8fdc06a5ed9a338720
dav-sh <dav.llwyr@gmail.com>
2023-11-02 17:55:42 -0600

    context_proccesor for the total of the cart added

---
+++ Shoppingcart/context_processor.py
@@ -0,0 +1,11 @@
+#similar to a global variable and it is registered in settings
+def total_cart(request):
+    total = 0
+
+    if request.user.is_authenticated:
+        for key, value in request.session['sh_cart'].items():
+            total = total + (float(value['product_price']*value['product_quantity']) )
+
+    return {'total_cart': total}
+
+
