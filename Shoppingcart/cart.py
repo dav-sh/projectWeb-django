7f524845f62a5b92c75d98f70386d38bd59e1d33
dav-sh <dav.llwyr@gmail.com>
2023-11-03 22:14:21 -0600

    authentication app created, and fixed kinda bug with cart variables

--- Shoppingcart/cart.py
+++ Shoppingcart/cart.py
@@ -4,22 +4,22 @@ class Cart:
         self.session = request.session
         carrito = self.session.get('sh_cart') #obtain the actual shopping cart
         if not carrito:
-            self.cart = self.session['sh_cart'] = {} #obtain the object sh_cart, create a {} dictionary and assing this to the sh_cart obtained
+            self.carrito = self.session['sh_cart'] = {} #obtain the object sh_cart, create a {} dictionary and assing this to the sh_cart obtained
         else:
-            self.cart = carrito #continue using the same cart object
+            self.carrito = carrito #continue using the same cart object
         
     def add_product(self, product):
-        if (str(product.id) not in self.cart.keys()):
-            self.cart[product.id] = {
+        if (str(product.id) not in self.carrito.keys()):
+            self.carrito[product.id] = {
                 'product_id': product.id,
                 'product_name': product.name,
                 'product_price': str(product.price),
                 'product_quantity': 1,
-                'product_image': product.image.url,
+                #'product_image': product.image.url,
             }
         else:
             #self.cart[product.id]['product_quantity'] += 1 
-            for key, value in self.cart.items():
+            for key, value in self.carrito.items():
                 if key == str(product.id):
                     value['product_quantity'] += 1
                     break
@@ -29,20 +29,20 @@ class Cart:
     
 
     def save_cart(self):
-        self.session['sh_cart'] = self.cart
+        self.session['sh_cart'] = self.carrito
         self.session.modified = True
 
 
     def delete_product(self, product):
         product.id = str(product.id)
 
-        if product.id in self.cart:
-            del self.cart[product.id]
+        if product.id in self.carrito:
+            del self.carrito[product.id]
             self.save_cart()
 
 
     def substract_product(self, product):
-        for key, value in self.cart.items():
+        for key, value in self.carrito.items():
                 if key == str(product.id):
                     value['product_quantity'] -= 1
                     if value['product_quantity'] <1:
--- Shoppingcart/context_processor.py
+++ Shoppingcart/context_processor.py
@@ -2,9 +2,9 @@
 def total_cart(request):
     total = 0
 
-    if request.user.is_authenticated:
-        for key, value in request.session['sh_cart'].items():
-            total = total + (float(value['product_price'])*value['product_quantity']) 
+    #if request.user.is_authenticated:
+    for key, value in request.session['sh_cart'].items():
+        total = total + (float(value['product_price'])*value['product_quantity']) 
 
     return {'total_cart': total}
 
