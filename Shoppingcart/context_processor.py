c54bf4a25a89bc2d1e52a94705baf0b708f1aa0a
dav-sh <dav.llwyr@gmail.com>
2023-11-06 16:42:31 -0600

    django form fixed and working properly

--- Shoppingcart/context_processor.py
+++ Shoppingcart/context_processor.py
@@ -3,8 +3,8 @@ def total_cart(request):
     total = 0
 
     #if request.user.is_authenticated:
-    for key, value in request.session['sh_cart'].items():
-        total = total + (float(value['product_price'])*value['product_quantity']) 
+    # for key, value in request.session['sh_cart'].items():
+    #     total = total + (float(value['product_price'])*value['product_quantity']) 
 
     return {'total_cart': total}
 
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 # Create your views here.
 
@@ -10,6 +12,19 @@ class RegisterView(View):
         return render(request, 'auth/auth.html', {'form': form})
     
     def post(self, request):
-        pass
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            # add the info in the table auth
+            
+            user = form.save()
 
+            login(request, user)
+
+            return redirect('home')
+
+        else:
+            for msg in form.error_messages:
+                messages.error(request, form.error_messages[msg])
+            return render(request, 'auth/auth.html', {'form': form})
 
