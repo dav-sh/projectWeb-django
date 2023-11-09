f08725f67235bc7d19601be2f93d134f329e5120
dav-sh <dav.llwyr@gmail.com>
2023-11-08 18:02:46 -0600

    implementing mailing

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Order, OrderDetail
+from Shoppingcart.cart import Cart
+
 
 # Create your views here.
+@login_required(login_url = '/auth/login')
+def proccess_order(request):
+    order = Order.objects.create(user = request.user)
+    cart = Cart(request)
+    orders = list()
+    for key,value in cart.objects.items():
+        orders.append(OrderDetail(
+            product_id = key,
+            quantity = value ['quantity'],
+            user = request.user,
+            order = order
+
+
+        ))
